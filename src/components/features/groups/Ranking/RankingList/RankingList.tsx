6fe5e3b6cbfd01a4fb9d9aa7b5f6acdd443552fb
baegyeong <beth0929@naver.com>
2026-08-11 21:52:17 +0900

    refactor: 랭킹 empty state를 별도 컴포넌트로 분리
    - RankingList의 empty state를 RankingEmptyState로 추출
    - RankingList는 API 훅 임포트 체인 때문에 스토리북에서 못 써서, 스토리·크로마틱 확인용으로 분리

--- src/components/features/groups/Ranking/RankingList/RankingList.tsx
+++ src/components/features/groups/Ranking/RankingList/RankingList.tsx
@@ -6,6 +6,7 @@ import type { SortValue } from '../../constants/groupSort';
 import type { GroupType } from '../../constants/groupType';
 import type { GroupFriendItem } from '../../queries/useGroupFriendListQuery';
 import { useGroupFriendListQuery } from '../../queries/useGroupFriendListQuery';
+import RankingEmptyState from '../RankingEmptyState/RankingEmptyState';
 import RankingItem from '../RankingItem/RankingItem';
 
 interface RankingListProps {
@@ -60,16 +61,7 @@ const RankingList = ({
   }
 
   if (data.length === 0) {
-    return (
-      <section className="flex h-28 flex-col items-center justify-center gap-1">
-        <p className="font-body-s text-g-0">아직 함께하는 멤버가 없어요</p>
-        {activeTab === 'FRIEND' && (
-          <p className="font-caption-n text-g-80">
-            친구를 초대하고 함께 기록해보세요
-          </p>
-        )}
-      </section>
-    );
+    return <RankingEmptyState activeTab={activeTab} />;
   }
 
   return (
