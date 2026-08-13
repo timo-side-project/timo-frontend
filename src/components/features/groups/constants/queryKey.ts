cd752417887bd66b7a8fe0e6ea58d8d26122eb9a
jeongyou <wjddbwjd1205@naver.com>
2026-08-13 22:07:33 +0900

    feat: 그룹 수정·삭제·탈퇴 엔드포인트와 쿼리 키 추가

--- src/components/features/groups/constants/queryKey.ts
+++ src/components/features/groups/constants/queryKey.ts
@@ -5,6 +5,10 @@ export const groupKeys = {
   list: () => [...groupKeys.all(), 'list'] as const,
   friendList: (groupId: number, sort: SortValue) =>
     [...groupKeys.all(), 'friendList', groupId, sort] as const,
+  detail: (groupId: number) => [...groupKeys.all(), 'detail', groupId] as const,
   create: () => [...groupKeys.all(), 'create'] as const,
   joinGroup: () => [...groupKeys.all(), 'joinGroup'] as const,
+  update: (groupId: number) => [...groupKeys.all(), 'update', groupId] as const,
+  delete: (groupId: number) => [...groupKeys.all(), 'delete', groupId] as const,
+  leave: (groupId: number) => [...groupKeys.all(), 'leave', groupId] as const,
 };
