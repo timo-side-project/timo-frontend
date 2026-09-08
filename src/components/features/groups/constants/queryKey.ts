f804736ee236da3200bfb887ea01cfe9bc8c5553
jeongyou <wjddbwjd1205@naver.com>
2026-09-08 21:41:00 +0900

    feat: 그룹 키워드 조회 엔드포인트와 쿼리 키 추가

--- src/components/features/groups/constants/queryKey.ts
+++ src/components/features/groups/constants/queryKey.ts
@@ -7,6 +7,8 @@ export const groupKeys = {
     [...groupKeys.all(), 'friendList', groupId, sort] as const,
   friendListByGroup: (groupId: number) =>
     [...groupKeys.all(), 'friendList', groupId] as const,
+  keywords: (groupId: number) =>
+    [...groupKeys.all(), 'keywords', groupId] as const,
   create: () => [...groupKeys.all(), 'create'] as const,
   joinGroup: () => [...groupKeys.all(), 'joinGroup'] as const,
   update: (groupId: number) => [...groupKeys.all(), 'update', groupId] as const,
